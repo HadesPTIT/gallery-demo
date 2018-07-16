57f1b70c51c541d21458a991ac4188616e8e95e4
Hades <Hphamptit@gmail.com>
2018-07-16 15:54:00 +0700

    Update Splash Screen

---
+++ app/src/main/java/com/framgia/gallerydemo/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.framgia.gallerydemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DURATION = 1000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },SPLASH_DURATION);
+
+    }
+}
